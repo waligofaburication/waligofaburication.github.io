b9c71607a1117d83f86063a7c6f5605999a5a2f9
waligofaburication <152712314+waligofaburication@users.noreply.github.com>
2023-12-02 18:45:12 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# WALIGOANDSONS.github.io
---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    var s_round = '.container2';
+  
+  
+    $(s_round).click(function() {
+      $('.container2').toggleClass('flipped1');
+      
+      return false;
+    });
+  
+  });
